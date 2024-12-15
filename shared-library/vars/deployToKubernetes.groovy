d545ff21057e91517c46bd244cf45242922af3b7
Ahmed Mahmoud <111567546+Ahmed-Jim1@users.noreply.github.com>
2024-12-15 23:15:43 +0200

    update

--- shared-library/vars/deployToKubernetes.groovy
+++ shared-library/vars/deployToKubernetes.groovy
@@ -1,9 +1,14 @@
 // vars/deployToKubernetes.groovy
 
 def call(String namespace) {
-    // Deploy to Kubernetes using the provided namespace
-    sh """
-        kubectl apply -f k8s-deployment.yaml --namespace=${namespace}
-        kubectl rollout status deployment/your-app --namespace=${namespace}
-    """
+    // Use credentials to fetch the Kubernetes kubeconfig
+    withCredentials([file(credentialsId: 'aks', variable: 'KUBECONFIG')]) {
+        // Deploy to Kubernetes using the provided namespace
+        sh """
+            export KUBECONFIG=${KUBECONFIG}
+            kubectl apply -f deployment.yaml --namespace=${namespace}
+            kubectl rollout status deployment/your-app --namespace=${namespace}
+        """
+    }
 }
+
