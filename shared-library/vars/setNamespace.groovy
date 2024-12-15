b3632f51f1c71b017ed82c7d66e9c409bb7a6eb5
Ahmed Mahmoud <111567546+Ahmed-Jim1@users.noreply.github.com>
2024-12-15 23:16:43 +0200

    .

--- shared-library/vars/setNamespace.groovy
+++ shared-library/vars/setNamespace.groovy
@@ -3,11 +3,11 @@
 def call(String branchName) {
     def namespace = ""
 
-    if (branchName.contains("dev")) {
+    if (branchName.contains("Dev")) {
         namespace = "dev"
-    } else if (branchName.contains("test")) {
+    } else if (branchName.contains("Test")) {
         namespace = "test"
-    } else if (branchName == "main" || branchName.contains("prod")) {
+    } else if (branchName == "main" || branchName.contains("Prod")) {
         namespace = "prod"
     } else {
         error "Branch name '${branchName}' does not match any known namespace"
